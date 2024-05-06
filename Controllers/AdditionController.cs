ef148a4e735168fe890b92e4d8838648384b918b
Deepak Nanaware <deepak@etrmservices.com>
2024-05-06 18:22:28 +0530

    git init

---
+++ Controllers/AdditionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+
+        private readonly IConfiguration? _configuration;
+
+        public AdditionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int sum = num1+ num2;
+            return Ok(sum);
+        }
+
+        [HttpGet("environment")]
+        public IActionResult GetEnvironment()
+        {
+            var env = _configuration.GetSection("Environment").Value;
+            Console.WriteLine("env=>",env);
+            return Ok(env);
+           /* string environment = _configuration?["Environment"] ?? "DefaultEnvironment"; // Handle null configuration
+           
+            return Ok(environment);*/
+        }
+    }
+}
---
+++ Model/AdditionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Model
+{
+    public class AdditionModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
